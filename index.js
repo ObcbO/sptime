6800a5aae1206ae9f742ee59af4c376b23ed4b75
ObcbO <m17089494917@163.com>
2023-07-28 20:29:03 +0800

    排序错误版 但能自动更新和剩余时间

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const table = document.getElementById("timeTable");
 
+function sortTable(servers) {
+    var rowCount = table.rows.length;
+    // 保留第一行
+    for (var i = rowCount - 1; i > 0; i--) {
+        table.deleteRow(i);
+    }
+
+    const sortedTimes = new Map([...servers.entries()].sort((a, b) => a[1] - b[1]));
+    for (const [key, value] of sortedTimes) {
+        const nextGiftTime = getNextGiftTime(value);
+
+        const row = table.insertRow(-1);
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
+        const cell3 = row.insertCell(2);
+        cell1.innerHTML = key;
+        cell2.innerHTML = new Date(nextGiftTime).toLocaleString();
+        cell3.innerHTML = `${(nextGiftTime - Date.now()) / 1000}s`;
+    }
+}
+
 function getNextGiftTime(timestamp) {
     const giftInterval = 20 * 60 * 1000; // 20分钟转化为毫秒
     const now = Date.now();
@@ -13,36 +34,33 @@ function getNextGiftTime(timestamp) {
     return nextGiftTime;
 }
 
-fetch('https://athena.wynntils.com/cache/get/serverList', {
-    method: "GET",
-    mode: 'cors',
-    timeout: 5000, // 设置5秒超时
-    headers: {
-        "Content-Type": "application/json",
-        "User-Agent": "Awa/0.1"
-    }
-}).then(response => {
+async function init() {
+    const response = await fetch('https://athena.wynntils.com/cache/get/serverList', {
+        method: "GET",
+        mode: 'cors',
+        timeout: 5000,
+        headers: {
+            "Content-Type": "application/json",
+            "User-Agent": "Awa/0.1"
+        }
+    });
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
-    return response.json();
-})
-    .then(data => {
-        const servers = data.servers;
-        const times = new Map();
-        Object.keys(servers).forEach(server => {
-            if (server !== 'YT') {
-                let nextGiftTime = getNextGiftTime(servers[server].firstSeen);
-                times.set(server, nextGiftTime);
-            }
-        })
-        // value升序
-        const sortedTimes = new Map([...times.entries()].sort((a, b) => a[1] - b[1]));
-        for (const [key, value] of sortedTimes) {
-            const row = table.insertRow(-1);
-            const cell1 = row.insertCell(0);
-            const cell2 = row.insertCell(1);
-            cell1.innerHTML = key;
-            cell2.innerHTML = new Date(value).toLocaleString();
+    const data = await response.json();
+    const servers = data.servers;
+    const times = new Map();
+    Object.keys(servers).forEach(server => {
+        if (server !== 'YT') {
+            times.set(server, servers[server].firstSeen);
         }
-    })
+    });
+    return times;
+}
+
+async function main() {
+    const servers = await init();
+    setInterval(() => sortTable(servers), 1000);
+}
+
+main();
